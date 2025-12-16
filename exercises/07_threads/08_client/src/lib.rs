04e1edf1624ebc8ade6f31fa63e1e8702f086a78
Jin <jin@Jins-MacBook-Air.local>
2025-12-16 15:01:42 +0100

    channelled client

--- exercises/07_threads/08_client/src/lib.rs
+++ exercises/07_threads/08_client/src/lib.rs
@@ -7,23 +7,41 @@ pub mod store;
 
 #[derive(Clone)]
 // TODO: flesh out the client implementation.
-pub struct TicketStoreClient {}
+pub struct TicketStoreClient {
+    command_sender: Sender<Command>,
+}
 
 impl TicketStoreClient {
     // Feel free to panic on all errors, for simplicity.
     pub fn insert(&self, draft: TicketDraft) -> TicketId {
-        todo!()
+        let (response_sender, response_receiver) = std::sync::mpsc::channel();
+    
+        let command = Command::Insert {
+            draft,
+            response_channel: response_sender,
+        };
+        
+        self.command_sender.send(command).expect("Server thread died");
+        response_receiver.recv().expect("Server didn't respond")
     }
 
     pub fn get(&self, id: TicketId) -> Option<Ticket> {
-        todo!()
+        let (response_sender, response_receiver) = std::sync::mpsc::channel();
+    
+        let command = Command::Get {
+            id,
+            response_channel: response_sender,
+        };
+        
+        self.command_sender.send(command).expect("Server thread died");
+        response_receiver.recv().expect("Server didn't respond")
     }
 }
 
 pub fn launch() -> TicketStoreClient {
     let (sender, receiver) = std::sync::mpsc::channel();
     std::thread::spawn(move || server(receiver));
-    todo!()
+    TicketStoreClient { command_sender: sender }
 }
 
 // No longer public! This becomes an internal detail of the library now.
